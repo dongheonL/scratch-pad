c96dff9d54bae184a3c8c922afaef08da798ce38
dongheonL <109117562+dongheonL@users.noreply.github.com>
2022-08-23 16:27:56 +0900

    HashMap

--- Main.java
+++ Main.java
@@ -1,55 +1,37 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 class Main{
     public static void main(String[] args) {
 
+        List<Integer> ageList = new ArrayList<>();
+        ageList.add(20); //철수
+        ageList.add(23); //수민
+        ageList.add(10); //영희
+        ageList.add(15); // 영수
 
+        System.out.println(ageList.get(1));
 
-        List<String> articles = new ArrayList<>();
-        // 리스트에 여러개가 들어가니 변수이름은 복수로 지어줘야함
-        // add함수 : list에 값 추가
-        articles.add("hello");
-        articles.add("bye");
-        articles.add("good");
-        articles.add("반가워요");
+        HashMap<String,Integer> ageMap = new HashMap<String,Integer>();
+        ageMap.put("철수",20); //철수
+        ageMap.put("수민",23); //수민
+        ageMap.put("영희",10); //영희
+        ageMap.put("영수",15); //영수
 
-        // get 함수 : list 안에 있는 데이터 가져오기
-        for(int i=0;i< articles.size();i++){
-            System.out.println(articles.get(i));
-        }
-        // set 함수 : list에 값 수정(index, 수정 할 데이터);
-        articles.set(1,"잘가세요");
-        System.out.println(articles.get(1));
-
-        // contains 함수 : lost 안에 값 유무 확인
-
-        System.out.println(articles.contains("어서오세요"));
-        System.out.println(articles.contains("반가워요"));
-
-        // indexOf 함수 : list 안에 값 위치 확인
-        System.out.println("값 위치 : "+ articles.indexOf("반가워요"));
-
-        //size() 함수 : ArrayList의 개수를 리턴
-        System.out.println("List size : " + articles.size());
-
-        // remove() 함수 : ArrauList에 데이터 삭제
-        // 방법 2개
-         articles.remove(0) ;// index로 접근하여 삭제
-         articles.remove("반가워요");  //값으로 접근하여 삭제
-
-        // 반복문 이용해서 list 순회
-        for(int i=0;i<articles.size();i++){
-            System.out.println(articles.get(i));
-        }
-
-
-
-
-
-
-
+        System.out.println("영희나이 :" +ageMap.get("영희"));
+        System.out.println("수민나이 :" +ageMap.get("수민"));
+        System.out.println("영수나이 :" +ageMap.get("영수"));
     }
-
-
 }
+
+// List
+// - 데이터 넣을 때 편하다.
+// - 인덱스가 자동 생성(우리한테 선택권이 없다)
+// - 데이터 가져올 때 불편하다.
+// - 데이터 넣는 함수 : add(데이터);
+
+// Map
+// - 데이터 넣을 때 불편하다.
+// - 인덱스가 수동 생성(우리가 지정해야 한다.)
+// - 데이터 넣는 함수 : put("데이터이름",데이터);
